4aa6f56149e727cc2bf4ae9f9645978adc4f6b72
Sagar-Rash <Sagar.Rash.7@gmail.com>
2025-08-06 15:49:34 -0400

    Update createLinReg.py

--- src/createLinReg.py
+++ src/createLinReg.py
@@ -5,12 +5,13 @@ def createLinReg(x_train, x_test, y_train, print_vals = False):
     # train linear model
     try:
         lrmodel = model.fit(x_train, y_train)
+        # Make predictions on train and test sets
+        lr_train_pred = lrmodel.predict(x_train)
+        lr_test_pred = lrmodel.predict(x_test)
     except Exception as e:
             print(f"Error occurred: {e}")
             return 0
-    # Make predictions on train and test sets
-    lr_train_pred = lrmodel.predict(x_train)
-    lr_test_pred = lrmodel.predict(x_test)
+    
     
     if print_vals:
         print("Model Coefficients:")
@@ -22,3 +23,4 @@ def createLinReg(x_train, x_test, y_train, print_vals = False):
         print("Number of Coefficients:")
         print(len(lrmodel.coef_))
     return lrmodel, lr_train_pred, lr_test_pred
+
