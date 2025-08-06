1405df7e335797f66e20353d40d51f7ef2ddd6e4
Sagar-Rash <Sagar.Rash.7@gmail.com>
2025-08-06 16:05:18 -0400

    Update createDTree.py

--- src/createDTree.py
+++ src/createDTree.py
@@ -3,15 +3,19 @@
 def createDTree(x_train, y_train, x_test, features, max_depth=3, max_features=10,random_state=567, plot_tree = False):
     # Create decision tree with set parameters given. max features = 10 by default
     from sklearn.tree import DecisionTreeRegressor
-    dt = DecisionTreeRegressor(max_depth=max_depth, max_features=max_features, random_state=random_state)
-    # Train    
-    dtmodel = dt.fit(x_train, y_train)
-    # Training Predictions
-    dt_train_pred = dtmodel.predict(x_train)
 
-    # Test Predictions
-    dt_test_pred = dtmodel.predict(x_test)
-
+    try:
+        dt = DecisionTreeRegressor(max_depth=max_depth, max_features=max_features, random_state=random_state)
+        # Train    
+        dtmodel = dt.fit(x_train, y_train)
+        # Training Predictions
+        dt_train_pred = dtmodel.predict(x_train)
+    
+        # Test Predictions
+        dt_test_pred = dtmodel.predict(x_test)
+    except Exception as e:
+            print(f"Error occurred: {e}")
+            return -1
     # If the user wants to plot the decision tree, plot the tree
     if plot_tree:
         from matplotlib.pyplot import show, savefig
@@ -21,9 +25,13 @@ def createDTree(x_train, y_train, x_test, features, max_depth=3, max_features=10
         features = features[0:max_features]
         tree.plot_tree(dtmodel, feature_names = features)
         # Save Figure
-        savefig(r'photos\CST2216_DTree.png', dpi=1000)
+        try:
+            savefig(r'photos\CST2216_DTree.png', dpi=1000)
         
-        show()
-        
+            show()
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return 0
     
     return dtmodel, dt_train_pred, dt_test_pred
+
